1c49fc703b54dc4773ff04e4197458872977c1c2
abhishekchopal159-star <abhishekchopal159@gmail.com>
2026-06-29 18:11:09 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { 
-  getAuth, 
-  GoogleAuthProvider, 
-  signInWithPopup, 
-  RecaptchaVerifier, 
-  signInWithPhoneNumber 
-} from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAcDOm0YQ0f5xKDqcPDJdILwj9LiOWFHC8",
   authDomain: "styloverse-6ab14.firebaseapp.com",
   projectId: "styloverse-6ab14",
   storageBucket: "styloverse-6ab14.firebasestorage.app",
-  messagingSenderId: "4705918734",
-  appId: "1:4705918734:web:303802d24d238dd581264d",
+  messagingSenderId: "47059187384",
+  appId: "1:47059187384:web:303802d24d238dd581264d",
   measurementId: "G-EF5SXDQRGJ"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-
-// Setting up standard configurations
-googleProvider.setCustomParameters({ prompt: 'select_account' });
-export { signInWithPopup, RecaptchaVerifier, signInWithPhoneNumber };
+const analytics = getAnalytics(app);
